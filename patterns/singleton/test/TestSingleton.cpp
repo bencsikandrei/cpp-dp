63157fc65c4d9ea2c9757d96040c4a26c616ef5f
Andrei BENCSIK <andrei.bencsik@gmail.com>
2018-07-15 16:14:32 +0200

    [CMake + Singleton][Addition] Constructed the base for the project
    Added all the necessary files for building the project.
    Made it as modular as possible using multiple CMakeLists.
    
    In the future all these design patterns will turn into a
    static library.

---
+++ patterns/singleton/test/TestSingleton.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN // catch provides a main function
+#include <catch2/catch.hpp>
+
+#include <Singleton_old.hpp>
+
+
+TEST_CASE("Catch works and this should fail", "[catch]") {
+	REQUIRE(false);
+}
+
